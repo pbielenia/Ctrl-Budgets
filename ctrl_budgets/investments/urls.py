d07b4e6feb405ec0cc3fdaf6258b8fa516f8501f
Piotr Bielenia <piotrbielenia0@gmail.com>
2024-12-13 22:44:43 +0100

    Remove investment budgets from the frontend

--- ctrl_budgets/investments/urls.py
+++ ctrl_budgets/investments/urls.py
@@ -5,10 +5,6 @@ from . import views
 app_name = 'investments'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('portfolio/new/', views.new_portfolio, name='new_portfolio'),
-    path('portfolio/<str:pk>/', views.portfolio, name='portfolio'),
-    path('portfolio/<str:portfolio_id>/asset-class/<str:asset_type_id>',
-         views.portfolio_element, name='portfolio_element'),
     path('targeted-budget/new/', views.targeted_budget_new, name='targeted_budget_new'),
     path('targeted-budget/<str:budget_id>/', views.targeted_budget, name='targeted_budget'),
 ]
