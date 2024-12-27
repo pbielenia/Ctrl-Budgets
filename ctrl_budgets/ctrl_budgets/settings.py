bd4ce524230e74777e9ce8afca2d760a6533dea7
Piotr Bielenia <piotrbielenia0@gmail.com>
2024-12-27 01:47:53 +0100

    [deploy] Split DJANGO_ALLOWED_HOSTS by space

--- ctrl_budgets/ctrl_budgets/settings.py
+++ ctrl_budgets/ctrl_budgets/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(' ')
 
 
 # Application definition
